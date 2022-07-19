a5dd815ec75a6faa4f73e9771414fbd576679fa2
longndt <longndt226@gmail.com>
2022-07-19 12:18:50 +0700

    web1

---
+++ ApplicationDevelopment/web1/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web1.Controllers
+{
+    public class DemoController : Controller
+    {
+        //function này dùng để render ra file Hello.cshtml trong thư mục Views/Demo
+        //Note1: Tên thư mục trong View trùng với tên Controller
+        //Note2: Tên file view trùng với tên function
+        //URL: /Demo/Hello
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ ApplicationDevelopment/web1/Controllers/GreenwichController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace web1.Controllers
+{
+    public class GreenwichController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Gửi data từ controller (back-end) sang view (front-end) bằng ViewData
+            string address = "2 Pham Van Bach - Cau Giay - Ha Noi";
+            int year = 2009;
+            ViewData["truong"] = "University of Greenwich"; ;
+            ViewData["diachi"] = address;
+            ViewData["nam"] = year;
+            //gửi data bằng ViewBag
+            double grade = 4.5;
+            ViewBag.Grade = grade;
+            List<string> students = new List<string>
+            {
+                "Minh", "Hoang", "Dung", "Phuong", "Trang"
+            };
+            students.Add("Tuan");
+            students.Add("Hong");
+            ViewBag.Students = students;
+            return View();
+        }
+    }
+}
