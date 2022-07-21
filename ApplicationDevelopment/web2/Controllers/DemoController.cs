547c4094642b497aa16187b55ec10d2b87b61448
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-07-21 10:13:08 +0700

    web

---
+++ ApplicationDevelopment/web2/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace web2.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Demo1()
+        {
+            string vn = "Vietnam is a peaceful country";
+            int year = 2022;
+            ViewData["vietnam"] = vn;
+            ViewData["year"] = year;
+            ViewData["grade"] = 7.8;
+            return View();
+        }
+
+        public IActionResult Demo2()
+        {
+            string gw = "Greenwich University - Vietnam";
+            ViewBag.Greenwich = gw;
+            ViewBag.Price = 999.99;
+            List<string> cities = new List<string>()
+            {
+                 "Hanoi", "HCM City", "Da Nang"
+            };
+            cities.Add("Hue");
+            cities.Remove("HCM City");
+            ViewBag.City = cities;        
+            return View();
+        }
+    }
+}
