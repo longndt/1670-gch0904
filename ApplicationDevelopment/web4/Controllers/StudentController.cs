ff960dced65abb5e5cec59b5e7814c526d34bf33
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-07-21 12:26:37 +0700

    web

---
+++ ApplicationDevelopment/web4/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using web4.Models;
+
+namespace web4.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult List()
+        {
+            //khởi tạo dữ liệu mẫu
+            Student s1 = new Student(1, "Nam", 18, "nam@gmail.com");
+            Student s2 = new Student(2, "Minh", 19, "minh@gmail.com");
+            Student s3 = new Student(3, "Huong", 20, "huong@gmail.com");
+            List<Student> students = new List<Student>();
+            students.Add(s1);
+            students.Add(s2);
+            students.Add(s3);
+            //render ra view và gửi kèm dữ liệu thông qua model
+            return View(students);
+        }
+    }
+}
---
+++ ApplicationDevelopment/web4/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace web4.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        public Student (int id, string name, int age, string email)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Email = email;
+        }
+    }
+}
