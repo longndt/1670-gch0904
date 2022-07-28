6e22c2c2b82a1b55bff7ce505945fa400050b676
longndt <longndt226@gmail.com>
2022-07-28 09:33:18 +0700

    web

---
+++ ApplicationDevelopment/web8/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using web8.Models;
+
+namespace web8.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //hàm này để render ra form cho người dùng nhập liệu
+        public IActionResult Input()
+        {
+            //mặc định thì 1 Action sẽ render ra View cùng tên với Action đấy
+            //View: Input.cshtml (nằm trong thư mục Views/Employee)
+            //return View();
+            //render ra View khác tên với Action 
+            return View("~/Views/Employee/Form.cshtml");
+        }
+
+        //hàm này để nhận dữ liệu từ form và render ra kết quả ở view khác
+        //Thông tin nhập từ form sẽ lưu vào model của Employee
+        public IActionResult Output(Employee employee)
+        {
+            //View: Output.cshtml (nằm trong thư mục Views/Employee)
+            //controller gửi dữ liệu qua view theo dạng model (strongly type)
+            return View(employee);
+        }
+    }
+}
---
+++ ApplicationDevelopment/web8/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web8.Models
+{
+    public class Employee
+    {
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Range(18,55)]
+        public int Age { get; set; }
+
+        [MinLength(5)]
+        [MaxLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+        public float Salary { get; set; }
+
+        public string Department { get; set; }
+
+        public string Strength { get; set; }
+     
+    }
+}
