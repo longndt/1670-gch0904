233320806af9f9f121f03a4a19804c7a2c1c56c8
longndt <longndt226@gmail.com>
2022-08-02 09:49:13 +0700

    web

--- ApplicationDevelopment/web9/Controllers/MobileController.cs
+++ ApplicationDevelopment/web9/Controllers/MobileController.cs
@@ -15,17 +15,32 @@ namespace web9.Controllers
         }
 
         //SQL: SELECT * FROM Mobile
-        //Mục đích: Load toàn bộ dữ liệu từ bảng Mobile và gửi về View
-        [Route("/")]
+        //set làm trang chủ của website bằng Route
+        [Route("/")]  
         public IActionResult Index()
         {
+            //lấy dữ liệu từ bảng Mobile và lưu vào list
             var mobiles = context.Mobile.ToList();
+            //render ra view và trả về dữ liệu từ DB
             return View(mobiles);
         }
 
         public IActionResult List()
         {
             return View(context.Mobile.ToList());
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            //check xem id có null không
+            if (id == null)
+            {
+                return NotFound();
+            }
+            //lấy dữ liệu của mobile theo id được gửi từ client-side (view)
+            var mobile = context.Mobile.FirstOrDefault(m => m.Id == id);
+            //trả về view cùng dữ liệu tương ứng
+            return View(mobile);
         }
     }
 }
