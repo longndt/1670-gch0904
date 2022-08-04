114cb1d914f0be76eab27a0d6812dc20d95a25c1
longndt <longndt226@gmail.com>
2022-08-04 11:25:57 +0700

    web

---
+++ ApplicationDevelopment/web10/Controllers/StaffController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using web10.Data;
+
+namespace web10.Controllers
+{
+    public class StaffController : Controller
+    {
+        //khai báo ApplicationDbContext để kết nối đến database
+        private readonly ApplicationDbContext context;  
+        
+        public StaffController (ApplicationDbContext DbContext)
+        {
+            context = DbContext;
+        }    
+
+        //view all staffs
+        public IActionResult Index()
+        {
+            var staffs = context.Staffs.ToList();  //array of Staff objects
+            return View(staffs); //gửi dữ liệu sang view bằng model
+        }
+
+        //view staff by id
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var staff = context.Staffs.Find(id);  //Staff object
+            return View(staff);
+        }
+    }
+}
